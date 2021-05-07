d96e5ef7986148913b2c30c6616746ab5ae8f99e
Exhale Yoga <exhaleyoga@exhales-air.lan>
2021-05-06 23:10:51 -0400

    added new endpoint for get shop by artist id

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,8 +1,15 @@
-const CorsMiddleware = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "POST, PUT, GET, DELETE, OPTIONS");
-    res.header('access-control-allow-headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
-    return next();
-  };
+// const CorsMiddleware = (req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "POST, PUT, GET, DELETE, OPTIONS");
+//     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     return next();
+//   };
   
-  module.exports = CorsMiddleware;
+//   module.exports = CorsMiddleware;
+
+  module.exports = (req, res, next) => {
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
+    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+};
