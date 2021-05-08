1b6fdc4ceabe0f1a8c8d5bfc975312f4e4e7d59e
Exhale Yoga <exhaleyoga@exhales-air.lan>
2021-05-08 11:06:17 -0400

    heroku server deployment

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ db.authenticate()
     console.log(e);
   });
 
-// app.listen(3000, function(){
-//     console.log('App is listening on port 3000')
-// })
+app.listen(process.env.PORT, () => {
+    console.log(`server is listening on port ${process.env.PORT}`)
+})
--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('redbadge-final-project', 'postgres', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 // const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //     dialect: 'postgres',
